04d78ebaa2947721f48b034657ab83695eca0a91
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2020-05-16 14:25:04 -0300

    Layout finalmente foi ajustado

--- mailsender/src/app/app.module.ts
+++ mailsender/src/app/app.module.ts
@@ -1,9 +1,11 @@
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {ToolbarModule} from 'primeng/toolbar';
-import {InputTextModule} from 'primeng/inputtext';
+import { ToolbarModule } from 'primeng/toolbar';
+import { InputTextModule } from 'primeng/inputtext';
+import { EditorModule } from 'primeng/editor';
+import { ButtonModule } from 'primeng/button';
 
 import { AppComponent } from './app.component';
 
@@ -15,7 +17,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     ToolbarModule,
-    InputTextModule
+    InputTextModule,
+    EditorModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
